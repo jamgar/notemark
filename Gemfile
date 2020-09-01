b24c3310aa482f46ef20b07062fa75cca4ed4c82
github username <github email address>
2020-09-01 12:29:07 -0500

    Add note

--- Gemfile
+++ Gemfile
@@ -54,3 +54,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "devise", "~> 4.7"
+
+gem "redcarpet", "~> 3.5"
+
+gem "coderay", "~> 1.1"
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,32 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("bootstrap");
+// var SimpleMDE = require("simplemde");
 
-
+import "../stylesheets/application.scss";
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+var simplemde;
+simplemde = null;
+
+document.addEventListener("turbolinks:before-cache", function () {
+  if (simplemde != null) {
+    simplemde.toTextArea();
+    simplemde = null;
+  }
+});
+
+document.addEventListener("turbolinks:load", function () {
+  simplemde = new SimpleMDE({
+    element: document.getElementById("note-textarea"),
+  });
+});
