6fd19fd618fff8d6c225c18c86eec93a4f7d80c7
github username <github email address>
2020-09-06 08:37:38 -0500

    Updated note cards

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import "../stylesheets/application.scss";
+
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
@@ -10,7 +12,6 @@ require("channels");
 require("bootstrap");
 // var SimpleMDE = require("simplemde");
 
-import "../stylesheets/application.scss";
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
